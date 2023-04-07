46ef7bc40f476baa2129330286e08f8b0d496db1
Stas Yaprintsev <21stason@gmail.com>
2023-04-07 12:30:18 +0300

    new deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'new deploy'
+
+git push -f git@github.com:sha9n/exchange-vue.git master:gh-pages
+
+cd -
